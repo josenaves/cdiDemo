c8e74c29f49fad5bf649416e7a52fce9b6b877e4
Jose Naves Moura Neto <jnaves@ig.com>
2012-08-21 09:22:25 -0300

    Passo 2.1: conflito de beans

---
+++ src/java/exemplo1/Alcool.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplo1;
+
+/**
+ *
+ * @author jnaves
+ */
+public class Alcool implements Combustivel {
+    public String toString() {
+        return "álcool";
+    }
+}
